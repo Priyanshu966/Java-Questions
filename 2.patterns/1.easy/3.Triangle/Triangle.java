0dc24863b97ba6cd978a58d8bfb9cbfd7264508e
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-01-31 15:33:59 +0530

    Add files via upload

---
+++ 2.patterns/1.easy/3.Triangle/Triangle.java
@@ -0,0 +1,14 @@
+// Print *    and  1   and  1   and 1   and  1   and 1
+//       **        22       21      12       23      23
+//       ***       333      321     123      345     456
+//       ****      4444     4321    1234     4567    78910
+  
+import java.util.Scanner;
+
+public class Triangle {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
