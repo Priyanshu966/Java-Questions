80d1c2258f483bcf50031a31ca71a7477769d89c
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-02-06 19:42:24 +0530

    Add files via upload

--- 1.conditional_and_loops/1.easy/16.Square-Root/SquareRoot.java
+++ 1.conditional_and_loops/1.easy/16.Square-Root/SquareRoot.java
@@ -1,23 +1,42 @@
 import java.util.Scanner;
 
 public class SquareRoot {
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     int temp = n;
+    //     int ans = 1;
+    //     boolean toTakePair = true;
+
+    //     while(temp > 1){
+    //         temp = temp / 2;
+    //         if(toTakePair){
+    //             ans *= 2; 
+    //             toTakePair = !toTakePair;
+    //         }else{
+    //             toTakePair = !toTakePair;
+    //         }
+    //     }
+    //     System.out.println(ans);
+    // }
+
+
+    //         Or
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int temp = n;
-        int ans = 1;
-        boolean toTakePair = true;
+        int output = 0;
 
-        while(temp > 1){
-            temp = temp / 2;
-            if(toTakePair){
-                ans *= 2; 
-                toTakePair = !toTakePair;
-            }else{
-                toTakePair = !toTakePair;
-            }
+        while(output * output < n){
+            output += 1;
         }
-        System.out.println(ans);
+        output -= 1;
+        System.out.println(output);
+
     }
+
+    
     
 }
