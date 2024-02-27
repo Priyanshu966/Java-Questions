5a062f2c04b5a45b62f983b90893c1f7714a7126
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2024-02-27 20:38:43 +0530

    Add files via upload

---
+++ 5.string/1.easy/5.Print-All-Substrings/PrintAllSubstrings.java
@@ -0,0 +1,54 @@
+// Print All Substrings
+
+// For a given input string(str), write a function to print all the possible substrings.
+// Substring
+// A substring is a contiguous sequence of characters within a string. 
+// Example: "cod" is a substring of "coding". Whereas, "cdng" is not as the characters taken are not contiguous
+// Input Format:
+// The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+// Output Format:
+// Print the total number of substrings possible, where every substring is printed on a single line and hence the total number of output lines will be equal to the total number of substrings.
+// Note:
+// The order in which the substrings are printed, does not matter.
+// Constraints:
+// 0 <= N <= 10^6
+// Where N is the length of the input string.
+
+// Time Limit: 1 second
+// Sample Input 1:
+// abc
+// Sample Output 1:
+// a 
+// ab 
+// abc 
+// b 
+// bc 
+// c 
+//  Sample Input 2:
+// co
+// Sample Output 2:
+// c 
+// co 
+// o
+
+import java.util.Scanner;
+
+public class PrintAllSubstrings {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i++){
+            String newStr = "";
+            newStr += str.charAt(i);
+            System.out.println(newStr); 
+            for(int j = i + 1; j < str.length(); j++){
+                newStr += str.charAt(j);
+                System.out.println(newStr);
+            }
+        }
+        
+    }
+    
+}
